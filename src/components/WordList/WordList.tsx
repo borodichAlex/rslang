f450c2d5ffb9da89354b297ac54b683d85ce123c
PavelMGs <myworkonets@gmail.com>
2021-04-03 13:39:00 +0300

    feat: add wordlist

--- src/components/WordList/WordList.tsx
+++ src/components/WordList/WordList.tsx
@@ -4,6 +4,9 @@ import { IWordObject } from '../../interfaces';
 import getData from '../../utils/getData';
 import s from './WordList.module.scss';
 import Sound from '../../assets/Sound.png';
+import Star from '../../assets/Star.png';
+import ActiveStar from '../../assets/ActiveStar.png';
+import Basket from '../../assets/Basket.png';
 
 type TGroup = 0 | 1 | 2 | 3 | 4 | 5;
 
@@ -30,13 +33,52 @@ const WordList = () => {
                 {
                     data?.map((item: IWordObject, index: number) => (
                     <div className={s.wordBlock} key={`${index}${item.word}list`}>
-                        {index + 1}. {item.word} - [{item.transcription}] - {item.wordTranslate}
-                        <br /> <br />
-                        <div style={{ marginLeft: '15px' }} dangerouslySetInnerHTML={{ __html: item.textMeaning }} />
+                        <button
+                            type="button"
+                            className={s.star_button}
+                        >
+                             <img src={true ? ActiveStar : Star} alt={true ? 'saved' : 'Save'} />        {/* условие, проверяющее наличие слова в сохранённых */}
+                        </button>
+                        <button
+                            type="button"
+                            className={s.basket_button}
+                        >
+                            <img src={Basket} alt="Delete" />
+                        </button>
+                        <div className={s.text_block}>
+                            {index + 1}. {item.word} - [{item.transcription}] - {item.wordTranslate}
+                            <button
+                                type="button"
+                                onClick={() => console.log('playSound(item.sound)')}
+                                className={s.word_translation_audio}
+                            >
+                                <img src={Sound} alt="PlaySound" />
+                            </button>
+                        </div>
+                        <br />
+                        <div className={s.text_block}>
+                            <span style={{ marginLeft: '15px' }} dangerouslySetInnerHTML={{ __html: item.textMeaning }} />
+                            <button
+                                    type="button"
+                                    onClick={() => console.log('playSound(item.sound)')}
+                                    className={s.word_translation_audio}
+                            >
+                                    <img src={Sound} alt="PlaySound" />
+                            </button>
+                        </div>
                         <br />
                         <div style={{ marginLeft: '15px' }}>{item.textMeaningTranslate}</div>
                         <br />
-                        <div style={{ marginLeft: '15px' }} dangerouslySetInnerHTML={{ __html: item.textExample }} />
+                        <div className={s.text_block}>
+                            <span style={{ marginLeft: '15px' }} dangerouslySetInnerHTML={{ __html: item.textExample }} />
+                            <button
+                                    type="button"
+                                    onClick={() => console.log('playSound(item.sound)')}
+                                    className={s.word_translation_audio}
+                            >
+                                    <img src={Sound} alt="PlaySound" />
+                            </button>
+                        </div>
                         <br />
                         <div style={{ marginLeft: '15px' }}>{item.textExampleTranslate}</div>
                     </div>))
