7164269dd0acf8be50f54b1e968de06c53365bbd
GrootStudent <borodichalexander97@gmail.com>
2021-04-04 15:18:35 +0300

    feat: add route game audioChallenge

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
+import GamePage from './pages/games/common/GamePage';
 import Menu from './shared/Menu';
+import {
+  Game as AudioChallenge,
+  description as DataAudioChallenge,
+} from './pages/games/audioChallenge';
+import styles from './stylesApp.module.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className={styles.root}>
         <Route path="/" component={Menu} />
+        <div className={styles.content}>
+          <Switch>
+            <Route exact path="/games/audioChallenge">
+              <GamePage Game={AudioChallenge} dataGame={DataAudioChallenge} />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
