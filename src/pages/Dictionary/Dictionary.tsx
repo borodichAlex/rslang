f5b930cf2708c3231177baa7b70eed5148af8011
PavelMGs <myworkonets@gmail.com>
2021-04-07 15:01:20 +0300

    feat: initial Dictionary

---
+++ src/pages/Dictionary/Dictionary.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+const Dictionary = () => {
+    useEffect(() => {
+        console.group('Dictionary');
+    }, []);
+
+    return (
+        <div>
+            ваф
+        </div>
+    )
+}
+
+export default Dictionary;
