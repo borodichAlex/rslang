de92aeaec52cb45a1518163efc593411a1008513
Stanislav Narckevich <azrae2197@gmail.com>
2021-04-17 03:53:13 +0300

    fix: fix video

--- src/shared/Video/Video.tsx
+++ src/shared/Video/Video.tsx
@@ -5,8 +5,8 @@ import s from './video.module.scss';
 const Video = ({ url }: { url: string }) => {
   const URL = `https://www.youtube.com/embed/${url}`;
   const { height, width } = useWindowDimensions();
-  const playerWidth = width > 1000 ? '1000px' : width - 50;
-  const playerHeight = width < 500 ? height / 2.5 : '600px';
+  const playerWidth = width > 1000 ? '800px' : width - 50;
+  const playerHeight = width < 500 ? height / 2.5 : '500px';
   return (
     <div className={s.video}>
       <iframe
