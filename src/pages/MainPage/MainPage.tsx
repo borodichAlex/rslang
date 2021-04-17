30a41c599c3c7b885b264e43ad5db3b02eea8135
Stanislav Narckevich <azrae2197@gmail.com>
2021-04-17 22:15:18 +0300

    feat: add info and description in main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,6 @@
 /*eslint-disable */
 import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
 import AboutTeam from '../../shared/AboutTeam/AboutTeam';
 import Video from '../../shared/Video/Video';
 import s from './MainPage.module.scss';
@@ -7,14 +8,30 @@ import s from './MainPage.module.scss';
 const MainPage = () => {
   return (
     <div className={s.root}>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos sint
-        veritatis sequi consequuntur et libero inventore magnam voluptates nemo
-        doloribus, dolore quos alias delectus doloremque expedita quidem
-        voluptas illo distinctio?
+      <div className={s.info}>
+        <Typography variant="h4" component="h4" className={s.description}>
+          RS Lang - это приложение, которое поможет тебе изучить английский язык
+          в игровой форме. У тебя будет возможность, прокачать свой лексический
+          запас английского с удовольствием, для этого в приложении есть такие
+          игры как: Спринт, Саванна, Аудиовызов и Конструктор слов, благодаря
+          которым ты узнашь много новых слов.Выбор уровня сложности в каждой
+          игре поможет освоиться как новичку так и мастеру английского. А также
+          ты сможешь наблюдать за своим прогрессом в разделе "Статистика" .
+          <Typography
+            variant="h4"
+            component="h4"
+            style={{ marginTop: '40px' }}
+          ></Typography>
+          Изучай английский с удовольствием!
+        </Typography>
+
+        <Video url="f4ioMGDQblI" />
+
+        <Typography variant="h2" component="h2" className={s.ourTeam}>
+          Наша команда
+        </Typography>
+        <AboutTeam />
       </div>
-      <Video url="f4ioMGDQblI" />
-      <AboutTeam />
     </div>
   );
 };
